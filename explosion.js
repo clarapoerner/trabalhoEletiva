3a035f46b45fb4d5f8a542c7e7d6e9563e1d3f7f
Clara Boamar <68236814+clarapoerner@users.noreply.github.com>
2023-06-11 22:54:08 -0300

    Update explosion.js
    texto colorido

--- explosion.js
+++ explosion.js
@@ -8,3 +8,23 @@ function perguntar() {
     document.getElementById("p").innerHTML = "VAI EMBORA!!!!!!!!!!";
   }
 }
+
+var header = document.querySelector("h1")
+
+function getRandomColor(){
+  var letters = "0123456789ABCDEF";
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random()*16)];
+  }
+  return color
+}
+
+//funçao
+function changeHeaderColor(){
+  colorInput = getRandomColor()
+  header.style.color = colorInput;
+}
+
+//milisegundos
+setInterval("changeHeaderColor()",500);
